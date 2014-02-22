0fc579c15c1d91504d9baae50aba5397d2680e1b
sophie.ramel <sophie@massen-ramel.net>
2014-02-22 21:53:00 +0100

    completed preferences with +/- buttons

--- src/org/soframel/alarm/decoder/settings/SettingsFragment.java
+++ src/org/soframel/alarm/decoder/settings/SettingsFragment.java
@@ -5,28 +5,32 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.view.*;
+import android.widget.*;
 import org.soframel.alarm.decoder.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: sophie
  * Date: 8/2/14
  */
-public class SettingsFragment extends PreferenceFragment implements View.OnClickListener {
+public class SettingsFragment extends PreferenceFragment implements View.OnClickListener{
+    //, ViewTreeObserver.OnGlobalLayoutListener
 
     public final static String TAG="SettingsFragment";
 
     private final static String KEY_NBLABELS="nbLabels";
     private final static String BUTTON_ADDTEXT="+";
-    private final static String BUTTON_REMOVETEXT="-";
+    protected final static String BUTTON_REMOVETEXT="-";
 
     private LinearLayout layout;
+    private LinearLayout buttonsLayout;
     private SharedPreferences sharedPrefs;
+
+    private Map<Integer, View> minusViews;
+    private Button addLabelButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,39 +38,120 @@ public class SettingsFragment extends PreferenceFragment implements View.OnClick
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        //buttonsFragment=new RemoveButtonsFragment(this);
+        //getFragmentManager().beginTransaction().replace(android.R.id.content,buttonsFragment).commit();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         layout = (LinearLayout) super.onCreateView(inflater, container, savedInstanceState);
+        //layout.getViewTreeObserver().addOnGlobalLayoutListener(this);
 
         sharedPrefs=this.getPreferenceManager().getSharedPreferences();
 
+        buttonsLayout=new LinearLayout(getActivity().getApplicationContext());
+        LinearLayout.LayoutParams buttonsParam = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        buttonsLayout.setLayoutParams(buttonsParam);
+        buttonsLayout.setOrientation(LinearLayout.VERTICAL);
+
+        ScrollView scrollView=new ScrollView(this.getActivity().getApplicationContext());
+        LinearLayout.LayoutParams scrollParam = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        scrollView.setLayoutParams(scrollParam);
+        FrameLayout fLayout=new FrameLayout(this.getActivity().getApplicationContext());
+        fLayout.addView(layout);
+        fLayout.addView(buttonsLayout);
+        scrollView.addView(fLayout);
+
+        minusViews =new HashMap<Integer, View>();
+
+        //filler
+        View filler=new View(this.getActivity().getApplicationContext());
+        LinearLayout.LayoutParams fillerParams = new LinearLayout.LayoutParams(
+                0,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        fillerParams.weight=1;
+        fillerParams.height=180; //TODO: find size of elements, do not use fixed size
+        filler.setLayoutParams(fillerParams);
+        buttonsLayout.addView(filler);
+
+
         //load existing labels
         int nbLabels=this.getNbLabels();
-        if(nbLabels>1){
-            for(int i=2;i<=nbLabels;i++){
+        if(nbLabels>0){
+            for(int i=1;i<=nbLabels;i++){
                 this.addLabel(i);
             }
         }
 
         //Add button
-        Button addLabelButton = new Button(getActivity().getApplicationContext());
+        addLabelButton = new Button(getActivity().getApplicationContext());
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity=Gravity.BOTTOM;
         addLabelButton.setLayoutParams(params);
         addLabelButton.setText(BUTTON_ADDTEXT);
-        layout.addView(addLabelButton);
+        buttonsLayout.addView(addLabelButton);
         addLabelButton.setOnClickListener(this);
 
-        return layout;
+        return scrollView;
+    }
+
+    private boolean buttonsNeedTranslation=true;
+
+    /*@Override
+    public void onGlobalLayout() {
+              if(buttonsNeedTranslation){
+                  this.translateMinusButtons();
+                  buttonsNeedTranslation=false;
+              }
+    }
+
+   private void translateMinusButtons(){
+        for(Integer i: minusViews.keySet()){
+            View minusView= minusViews.get(i);
+            float targetY=this.findPreferenceY(i);
+
+            minusView.setY(targetY);
+        }
+         Log.d(TAG, "Minus buttons moved");
+
+       //change y for add button
+       addLabelButton.setY(buttonsLayout.getHeight()-addLabelButton.getHeight());
+    }
+
+    protected float findPreferenceY(int i){
+        float z=0;
+
+        View child=layout.getChildAt(0);
+        if(child!=null && child instanceof ListView){
+            ListView prefsView=(ListView) child;
+            if(prefsView.getChildCount()>=i+1){
+                View nthChild=prefsView.getChildAt(i+1);
+                if(nthChild!=null && nthChild instanceof LinearLayout){
+                    z=nthChild.getY();
+                }
+            }
+
+        }
+
+        return z;
+    }      */
+
+    private String getLabelTitle(int i){
+         return getResources().getString(R.string.pref_mapping)+" "+i+":";
     }
 
     private void addLabel(int i){
         Log.i(TAG, "adding label");
         EditKeyValueTextPreference pref=new EditKeyValueTextPreference(this.getActivity());
-        pref.setTitle(R.string.pref_mapping);
+        pref.setTitle(this.getLabelTitle(i));
         String key="mapping"+i;
         pref.setKey(key);
 
@@ -80,21 +165,24 @@ public class SettingsFragment extends PreferenceFragment implements View.OnClick
         this.getPreferenceScreen().addPreference(pref);
 
         //add delete button
-        LinearLayout prefLayout=new LinearLayout(this.getActivity());
-        prefLayout.setId(1000+i);
-        prefLayout.setGravity(Gravity.RIGHT);
-        prefLayout.setTranslationY(-i*100);
-        layout.addView(prefLayout);
-
         Button button = new Button(getActivity().getApplicationContext());
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.topMargin=30;
+        params.bottomMargin=4;
+        params.gravity=Gravity.RIGHT;
         button.setLayoutParams(params);
         button.setId(i);
         button.setText(BUTTON_REMOVETEXT);
         button.setOnClickListener(this);
-        prefLayout.addView(button);
+        minusViews.put(i, button);
+
+        /*if(i>1)
+            buttonsLayout.addView(button, buttonsLayout.getChildCount()-2);
+        else
+            buttonsLayout.addView(button);*/
+        buttonsLayout.addView(button, i);
     }
 
     private void removeLabel(int i){
@@ -104,15 +192,36 @@ public class SettingsFragment extends PreferenceFragment implements View.OnClick
             this.getPreferenceScreen().removePreference(pref);
 
             //remove delete button
-            View buttonLayout=layout.findViewById(1000+i);
-            if(buttonLayout!=null){
-                layout.removeView(buttonLayout);
+            View button=buttonsLayout.findViewById(i);
+            if(button!=null){
+                buttonsLayout.removeView(button);
+            }
+            minusViews.remove(i);
+        }
+
+        //switch all other preferences and buttons !
+        for(int j=i+1;j<=this.getNbLabels();j++){
+              //switch preference
+            int newJ=j-1;
+            Preference otherPref=this.getPreferenceScreen().findPreference("mapping"+j);
+            if(otherPref!=null){
+                otherPref.setKey("mapping"+newJ);
+                otherPref.setTitle(this.getLabelTitle(newJ));
+            }
+
+            //switch remove button
+            View button=minusViews.get(j);
+            if(button!=null){
+                button.setId(newJ);
+                minusViews.put(newJ, button);
+                minusViews.remove(j);
             }
         }
+        this.buttonsNeedTranslation=true;
     }
 
     public int getNbLabels(){
-        return sharedPrefs.getInt(KEY_NBLABELS, 1);
+        return sharedPrefs.getInt(KEY_NBLABELS, 0);
     }
     public void setNbLabels(int nb){
         SharedPreferences.Editor editor=sharedPrefs.edit();
@@ -126,6 +235,7 @@ public class SettingsFragment extends PreferenceFragment implements View.OnClick
                   Button b=(Button) v;
                 String text=b.getText().toString();
                 if(text.equals(BUTTON_ADDTEXT)){
+                    buttonsNeedTranslation=true;
                     int nbLabels=getNbLabels();
                     nbLabels++;
                     this.addLabel(nbLabels);
@@ -133,13 +243,15 @@ public class SettingsFragment extends PreferenceFragment implements View.OnClick
                 }
                 if(text.equals(BUTTON_REMOVETEXT)){
                     //find which label we're talking about and remove it
+                    buttonsNeedTranslation=true;
                     int i=b.getId();
-                    Log.d(TAG, "Remove button clicked: "+i);
                     int nbLabels=getNbLabels();
                     this.removeLabel(i);
                     nbLabels--;
                     setNbLabels(nbLabels);
+
                 }
             }
     }
+
 }
